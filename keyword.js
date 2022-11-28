1928a0c5bf6262cf4737d1bc2e0589189095d64c
ダニー <98234578+mrdannyarmstrong@users.noreply.github.com>
2022-11-28 14:21:52 -0500

    Add files via upload

--- keyword.js
+++ keyword.js
@@ -1,21 +1,21 @@
 function findkeyword() {
 	cururl = window.location.toString();
-	keyword = cururl.replace('https://tokkisearch.github.io/web/?find=', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/web', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/img/?find=', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/img', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/vid/?find=', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/vid', "");
-	keyword = keyword.replace('https://tokkisearch.github.io/news/?find=', ""); 
-	keyword = keyword.replace('https://tokkisearch.github.io/news', "");
+	keyword = cururl.replace("https://tokkisearch.github.io/web/?find=", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/web", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/img/?find=", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/img", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/vid/?find=", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/vid", "");
+	keyword = keyword.replace("https://tokkisearch.github.io/news/?find=", ""); 
+	keyword = keyword.replace("https://tokkisearch.github.io/news", "");
 	keyword = keyword.toLowerCase();
 	keyword = keyword.replace(" ", "+")
-	sugin.value = cururl.replace('https://tokkisearch.github.io/web/?find=', "");
-	sugin.value = sugin.value.replace('https://web.westfield.ma.edu/tokki/web', "");
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/img/?find=', ""); 
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/img', ""); 
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/vid/?find=', ""); 
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/vid', "");
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/news/?find=', ""); 
-	sugin.value = sugin.value.replace('https://tokkisearch.github.io/news', "");
+	sugin.value = cururl.replace("https://tokkisearch.github.io/web/?find=", "");
+	sugin.value = sugin.value.replace("https://web.westfield.ma.edu/tokki/web", "");
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/img/?find=", ""); 
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/img", ""); 
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/vid/?find=", ""); 
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/vid", "");
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/news/?find=", ""); 
+	sugin.value = sugin.value.replace("https://tokkisearch.github.io/news", "");
 }
